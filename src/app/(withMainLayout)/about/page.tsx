125f4ab4acf16618c93945ba7e581a612753a15d
naimurk <naimurk781@gmail.com>
2025-02-03 15:22:47 +0600

    foolder structure

--- src/app/(withMainLayout)/about/page.tsx
+++ src/app/(withMainLayout)/about/page.tsx
@@ -1,5 +1,5 @@
 const page = () => {
-  return <div>hello world</div>;
+  return <div></div>;
 };
 
 export default page;
---
+++ src/app/(withMainLayout)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default MainLayout;
