b46410e672595835c7aac55840e7d6c94509ea3d
naimurk <naimurk781@gmail.com>
2025-02-04 10:42:29 +0600

    font family added

--- src/app/(withMainLayout)/page.tsx
+++ src/app/(withMainLayout)/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div className="bg-gradient-spectrum   h-screen"></div>;
+  return (
+    <div className="bg-gradient-spectrum   h-screen">
+      <h1 className="text-3xl">hello world</h1>
+    </div>
+  );
 };
 
 export default Home;
