8c9142f3f993c7ed22df8540410adeddef80e567
naimur-reza <naimur.rezaa@gmail.com>
2025-02-05 14:41:17 +0600

    Update WhoWeAre component styles for improved layout and readability; add padding to container

--- src/app/(withMainLayout)/home/_components/WhoWeAre.tsx
+++ src/app/(withMainLayout)/home/_components/WhoWeAre.tsx
@@ -28,15 +28,19 @@ const WhoWeAre = () => {
           </div>
         ))}
 
-        <div className="relative bg-primary p-4 md:p6 lg:p-7 !pb-0 rounded-3xl text-white    md:col-span-3 lg:col-span-1 min-h-[400px] md:min-h-[460px] lg:min-h-auto">
+        <div className="relative bg-primary p-4 md:p6 lg:p-7 !pb-0 rounded-3xl text-white    md:col-span-3 lg:col-span-1 min-h-[300px] md:min-h-[460px] lg:min-h-auto">
           <h1 className="text-xl font-medium mb-2">Need Help?</h1>
-          <p className="text-3xl md:text-2xl font-medium">Free Advice.</p>
-          <p className="text-3xl md:text-2xl font-medium">Book a callback</p>
+          <p className="text-3xl md:text-5xl lg:text-2xl font-medium">
+            Free Advice.
+          </p>
+          <p className="text-3xl md:text-5xl lg:text-2xl font-medium">
+            Book a callback
+          </p>
           <div className=" w-full ">
             <Image
               height={423}
               width={300}
-              className=" absolute   max-w-55 right-1/2 translate-x-1/2 bottom-0 "
+              className=" absolute max-w-40 md:max-w-70   lg:max-w-46 right-1/2 translate-x-1/2 bottom-0 "
               src={assets.images.phoneGirl}
               alt="Who we are"
             />
