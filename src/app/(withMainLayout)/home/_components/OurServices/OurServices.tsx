f14be7c3dcef47184a39f3396de535971c3c2642
naimur-reza <naimur.rezaa@gmail.com>
2025-02-10 17:30:21 +0600

    Enhance visual design of OurServices component with background layers and improved layout

--- src/app/(withMainLayout)/home/_components/OurServices/OurServices.tsx
+++ src/app/(withMainLayout)/home/_components/OurServices/OurServices.tsx
@@ -2,12 +2,17 @@ import { SectionTitle } from "@/components/common";
 
 const OurServices = () => {
   return (
-    <div className="container border pt-8 md:pt-10 lg:pt-14 bg-gradient-purple rounded-card ">
-      <SectionTitle
-        color="text-white"
-        buttonText="Our services"
-        title="Digital services to grow your business"
-      />
+    <div className="container relative border pt-8 md:pt-10 lg:pt-14 rounded-card overflow-hidden">
+      <div className="absolute inset-0 bg-[#0A0F42]  rounded-card" />
+      <div className="absolute w-[600px] h-[350px] -top-1/2 left-1/2 -translate-x-1/2 blur-[100px] bg-green-400/30 rounded-card" />
+
+      <div className="relative z-10">
+        <SectionTitle
+          color="text-white"
+          buttonText="Our services"
+          title="Digital services to grow your business"
+        />
+      </div>
     </div>
   );
 };
