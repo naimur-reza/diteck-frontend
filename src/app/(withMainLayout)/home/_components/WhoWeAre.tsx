e2b0821b9049776c46d944d4ba9c0c131d2b1cc3
naimur-reza <naimur.rezaa@gmail.com>
2025-02-10 12:46:54 +0600

    Adjust layout and styling in WhoWeAre component; update container width in globals.css

--- src/app/(withMainLayout)/home/_components/WhoWeAre.tsx
+++ src/app/(withMainLayout)/home/_components/WhoWeAre.tsx
@@ -13,11 +13,11 @@ const WhoWeAre = () => {
           marketing websites, innovative web apps and mobile applications."
         title="Your partners for digital success"
       />
-      <div className="grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 gap-8 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 gap-5 mt-8">
         {services.map((service) => (
           <div
             key={service.title}
-            className="p-4 md:p-6 lg:p-7 tracking-tight rounded-3xl bg-background   "
+            className="p-4 hover:-translate-y-2.5 transform duration-300 md:p-6 lg:p-7 tracking-tight rounded-3xl bg-background   "
           >
             <service.icon size={60} className="text-primary mb-4" />
             <h2 className="text-xl md:text-2xl lg:text-3xl font-medium mb-2 mt-10 md:mt-14 lg:mt-20">
@@ -29,7 +29,7 @@ const WhoWeAre = () => {
           </div>
         ))}
 
-        <div className="relative bg-primary p-4 md:p6 lg:p-7 !pb-0 rounded-3xl text-white    md:col-span-3 lg:col-span-1 min-h-[300px] md:min-h-[460px] lg:min-h-auto">
+        <div className="relative  bg-primary p-4 md:p6 lg:p-7 !pb-0 rounded-3xl text-white    md:col-span-3 lg:col-span-1 min-h-[300px] md:min-h-[460px] lg:min-h-auto">
           <h1 className="text-xl font-medium mb-2">Need Help?</h1>
           <p className="text-3xl md:text-5xl lg:text-2xl font-medium">
             Free Advice.
--- src/app/(withMainLayout)/home/page.tsx
+++ src/app/(withMainLayout)/home/page.tsx
@@ -9,11 +9,11 @@ const Home = () => {
     <div>
       <Hero />
       <WhoWeAre />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-10 my-12 px-12"></div>
       <WhyUs />
       <SelectedWorks />
       <RecentPost />
     </div>
   );
 };
+
 export default Home;
