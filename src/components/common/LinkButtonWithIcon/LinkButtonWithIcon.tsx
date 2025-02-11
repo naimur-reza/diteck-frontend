57168de5ed4610882789df3bfa817e9201f2d7af
Morshed-Office <morshed952640@gmail.com>
2025-02-11 17:28:37 +0600

    completed not-found page with responsive

--- src/components/common/LinkButtonWithIcon/LinkButtonWithIcon.tsx
+++ src/components/common/LinkButtonWithIcon/LinkButtonWithIcon.tsx
@@ -6,11 +6,13 @@ export const LinkButtonWithIcon = ({
   link,
   invertedBorder = true,
   position = "center",
+  textColor = "text-black",
 }: {
   text: string;
   link: string;
   invertedBorder?: boolean;
   position?: "start" | "center" | "end";
+  textColor?: string;
 }) => {
   const positioning = {
     start: "justify-start",
@@ -30,12 +32,16 @@ export const LinkButtonWithIcon = ({
           <div className="relative  flex items-center min-w-[180px] w-fit   h-12 overflow-hidden">
             <span className="flex items-center w-10 h-10 rounded-[10px] bg-[#5865F2]  transition-all duration-500 ease-in-out group-hover:w-full group-hover:justify-between px-3">
               <MoveRight className="text-white transition-transform duration-500 group-hover:translate-x-0" />
-              <span className="absolute right-5 opacity-0 translate-y-8 transition-all duration-400 ease-out group-hover:opacity-100 group-hover:translate-y-0 text-white">
+              <span
+                className={`absolute right-5 opacity-0 translate-y-8 transition-all duration-400 ease-out group-hover:opacity-100 group-hover:translate-y-0 text-white`}
+              >
                 {text ? text : "Get in touch"}
               </span>
             </span>
 
-            <p className="absolute right-3  transition-all  duration-300 ease-in-out  group-hover:-translate-y-8 group-hover:opacity-0">
+            <p
+              className={`absolute right-3  transition-all  duration-300 ease-in-out  group-hover:-translate-y-8 group-hover:opacity-0 ${textColor}`}
+            >
               {text ? text : "Get in touch"}
             </p>
           </div>
