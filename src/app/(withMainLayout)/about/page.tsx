59a8333e78280b816ef711aa202edfce31b54249
Morshed-Office <morshed952640@gmail.com>
2025-02-12 10:29:32 +0600

    complete and modify the ParallaxBanner and also responsive

--- src/app/(withMainLayout)/about/page.tsx
+++ src/app/(withMainLayout)/about/page.tsx
@@ -1,5 +1,16 @@
+import { ParallaxBanner } from "@/components/common";
+
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ParallaxBanner
+        img="https://demo2.wpopal.com/diteck/wp-content/uploads/2024/11/about_bc.jpg"
+        pageTitle="About Us"
+        title="We are Ena Ema"
+        description="We’re a team of expert designers, web developers and marketers who’ve been delivering digital success for more than a decade."
+      />
+    </div>
+  );
 };
 
 export default page;
