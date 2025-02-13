dd9197cc5bdaad07d30bd4077e6d3a9844a77b22
naimur-reza <naimur.rezaa@gmail.com>
2025-02-13 10:16:28 +0600

    Refactor Contact page component to include SectionTitle and enhance form layout

--- src/app/(withMainLayout)/contact/page.tsx
+++ src/app/(withMainLayout)/contact/page.tsx
@@ -1,6 +1,6 @@
-import { ParallaxBanner } from "@/components/common";
+import { ParallaxBanner, SectionTitle } from "@/components/common";
 
-const page = () => {
+const Contact = () => {
   return (
     <div className="container">
       <ParallaxBanner
@@ -8,9 +8,49 @@ const page = () => {
           "https://demo2.wpopal.com/diteck/wp-content/uploads/2024/11/blog_bc.jpg"
         }
         title="Contact Us"
+        pageTitle="Contact"
       />
+
+      <div className="mt-10">
+        <SectionTitle
+          title="We're ready to help you!"
+          buttonText="Get in touch"
+        />
+
+        <div className="md:grid grid-cols-5 py-10">
+          <div className="col-span-2"></div>
+          <div className="grid grid-cols-2 gap-4 col-span-3">
+            <input
+              className={inputStyle + "bg-white"}
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              className={inputStyle + "bg-white"}
+              type="text"
+              placeholder="Email"
+            />
+            <input
+              className={`${inputStyle} bg-white col-span-2`}
+              type="text"
+              placeholder="Subject"
+            />
+            <textarea
+              className={`${inputStyle} bg-white col-span-2`}
+              placeholder="Message"
+            />
+            <input
+              className={`${inputStyle} bg-primary col-span-2 hover:bg-primary/90 transition-colors cursor-pointer text-white`}
+              type="submit"
+              value="Submit"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Contact;
+
+const inputStyle = "rounded  px-4 py-3 rounded-2xl focus:outline-none w-full  ";
