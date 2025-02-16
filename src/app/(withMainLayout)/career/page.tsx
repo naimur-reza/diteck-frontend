c70b02c0547264701d06f06c0f37e4652784c9ca
naimur-reza <naimur.rezaa@gmail.com>
2025-02-16 16:31:26 +0600

    Feat: Add Reusable Modal with framer motion

--- src/app/(withMainLayout)/career/page.tsx
+++ src/app/(withMainLayout)/career/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import { ParallaxBanner, SectionTitle } from "@/components/common";
 import CareerCard from "./_components/CareerCard";
 
+import Modal from "@/components/ui/modal/Modal";
+import { useModal } from "@/hooks/useModal";
+
 const Career = () => {
+  const { isOpen, openModal, closeModal } = useModal();
+
   return (
     <div className="container bg-gradient-to-t from-transparent via-rose-100 to-transparent">
       <ParallaxBanner
@@ -14,9 +21,22 @@ const Career = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-7 py-10">
         {jobs.map((job, index) => (
-          <CareerCard key={index} {...job} />
+          <CareerCard openModal={openModal} key={index} {...job} />
         ))}
       </div>
+
+      <Modal isOpen={isOpen} onClose={closeModal} title="Example Modal">
+        <p className="mb-4">
+          This is a reusable modal component built with Tailwind CSS and
+          Next.js.
+        </p>
+        <button
+          onClick={closeModal}
+          className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+        >
+          Close Modal
+        </button>
+      </Modal>
     </div>
   );
 };
