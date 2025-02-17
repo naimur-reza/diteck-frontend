d9f8b97d4425a6dd422007bc6844ebe0264b5a9d
naimur-reza <naimur.rezaa@gmail.com>
2025-02-17 14:39:47 +0600

    Design fix

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -14,51 +14,56 @@ const Hero = () => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "700px",
       }}
-      className=" relative"
+      className=" relative "
     >
-      <div className="container relative bg-black/50 grid grid-cols-1 md:grid-cols-2  in-h-screen pt-30 md:pt-44 pb-12 md:pb-20 ">
-        <div className="absolute inset-0 -z-10 bg-linear-120 from-purple-900/90 via-primary to-purple-900/90" />
-        <div className="relative aspect-square md:h-[550px] md:w-[95%] ">
-          <Image
-            src="https://demo2.wpopal.com/diteck/wp-content/uploads/2024/12/h5_img-1.jpg"
-            alt="hero_image"
-            className="object-cover rounded-3xl"
-            fill
-          />
-        </div>
-        <section className="pt-10 md:pt-0 text-center md:text-start relative z-20">
-          <div className="flex flex-col gap-4 md:grid md:items-center">
-            <div className="col-span-2 flex justify-center md:block">
-              <PulseButton
-                color="text-white border-white/20"
-                buttonText="Hear from customer"
-              />
-            </div>
+      <div className="relative bg-black/50  pt-30 md:pt-44 pb-12">
+        {/* gradients */}
+        <div className="absolute inset-0 -z-10 bg-linear-120 from-purple-900/90  via-primary to-purple-900/90" />
 
-            <div className="col-span-3">
-              <div className="flex flex-col gap-6">
-                <h2 className={`section-title px-10 md:px-0 text-white`}>
-                  The digital agency built for brands
-                </h2>
-
-                <p
-                  className={`text-lg md:text-xl font-medium text-background lg:pr-20`}
-                >
-                  We’re a team of expert who’ve been delivering digital success
-                  for more than a decade.
-                </p>
-
-                <LinkButtonWithIcon
-                  textColor="text-white"
-                  className="justify-center md:justify-start text-white relative"
-                  invertedBorder={false}
-                  link="/"
-                  text="View Our Work"
+        {/* contents */}
+        <div className="grid grid-cols-1 md:grid-cols-2 container min-h-screen">
+          <div className="relative aspect-square md:h-[550px] md:w-[95%] ">
+            <Image
+              src="https://demo2.wpopal.com/diteck/wp-content/uploads/2024/12/h5_img-1.jpg"
+              alt="hero_image"
+              className="object-cover rounded-3xl"
+              fill
+            />
+          </div>
+          <section className="pt-10 md:pt-0 text-center md:text-start relative z-20">
+            <div className="flex flex-col gap-4 md:grid md:items-center">
+              <div className="col-span-2 flex justify-center md:block">
+                <PulseButton
+                  color="text-white border-white/20"
+                  buttonText="Hear from customer"
                 />
               </div>
+
+              <div className="col-span-3">
+                <div className="flex flex-col gap-6">
+                  <h2 className={`section-title px-10 md:px-0 text-white`}>
+                    The digital agency built for brands
+                  </h2>
+
+                  <p
+                    className={`text-lg md:text-xl font-medium text-background lg:pr-20`}
+                  >
+                    We’re a team of expert who’ve been delivering digital
+                    success for more than a decade.
+                  </p>
+
+                  <LinkButtonWithIcon
+                    textColor="text-white"
+                    className="justify-center md:justify-start text-white relative"
+                    invertedBorder={false}
+                    link="/"
+                    text="View Our Work"
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     </div>
   );
