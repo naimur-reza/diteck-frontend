ebeb95bfa444d519d0995f79d0c9ba5c58df24cf
naimur-reza <naimur.rezaa@gmail.com>
2025-02-26 09:43:44 +0600

    Refactor API setup: change base URL and update reducer path in baseApi

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,13 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
-  endpoints: (build) => ({
-    getPokemonByName: build.query({
-      query: (name: string) => `pokemon/${name}`,
-    }),
-  }),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+  endpoints: () => ({}),
 });
-
-export const { useGetPokemonByNameQuery } = baseApi;
