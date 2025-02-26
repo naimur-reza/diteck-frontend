033e8e3ab554b50eb1cfe512d6222d0bf43034e3
Morshed-Office <morshed952640@gmail.com>
2025-02-26 15:07:41 +0600

    Reusable Input field done

--- src/app/(withDashboardLayout)/dashboard/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -1,4 +1,27 @@
+"use client";
+import { EnaForm, EnaInput } from "@/components/forms";
+import { FieldValues } from "react-hook-form";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+
+  const handleLogin = async (data: FieldValues) => {
+    console.log(data);
+  }
+
+  return <div>
+    <EnaForm onSubmit={handleLogin} defaultValues={{ email: "" }}>
+
+      <EnaInput
+        name="email"
+        type="email"
+        placeholder="Enter your email"
+        className="mb-4"
+      />
+
+      <button type="submit">
+        Submit
+      </button>
+    </EnaForm>
+  </div>;
 };
 export default Dashboard;
