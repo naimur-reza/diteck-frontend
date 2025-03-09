1a6dfc0ce1c0a1b0093876d2192ae9b9def9316d
naimurk <naimurk781@gmail.com>
2025-03-09 12:37:22 +0600

    landing page project section error handeling done

--- src/app/(withMainLayout)/home/page.tsx
+++ src/app/(withMainLayout)/home/page.tsx
@@ -12,11 +12,11 @@ import OurServicesPage from "./_components/OurServices/OurServicesPage";
 const HomePage = () => {
   return (
     <div className="space-y-8 md:space-y-14 lg:space-y-20">
-      {/* <Hero /> */}
-      {/* <WhoWeAre /> */}
-      {/* <OurServicesPage /> */}
-      {/* <WhyUs /> */}
-      {/* <SelectedWorks /> */}
+      <Hero />
+      <WhoWeAre />
+      <OurServicesPage />
+      <WhyUs />
+      <SelectedWorks />
       {/* <TeamMember /> */}
       {/* <Testimonial /> */}
       {/* <RecentPost /> */}
--- src/utils/fetchData/getAllProjects.ts
+++ src/utils/fetchData/getAllProjects.ts
@@ -1,8 +1,17 @@
 const getAllProjects = async () => {
-
+  try {
     const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/previous-work`;
     const res = await fetch(url);
-    return res.json();
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch projects: ${res.statusText}`);
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    return { data: [] };
+  }
 };
 
 export default getAllProjects;
