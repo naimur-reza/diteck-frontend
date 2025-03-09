ecc9f8c8c937b3978057c80caec37358602f7e42
Morshed-Office <morshed952640@gmail.com>
2025-03-09 15:19:22 +0600

    fix: Validation error show properly in Hiring, Blogs and Projects and also add a new field category in blog

--- src/utils/fetchData/getAllReviews copy.ts
+++
@@ -1,8 +0,0 @@
-const getAllReviews = async () => {
-
-    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/review/all-reviews`;
-    const res = await fetch(url);
-    return res.json();
-};
-
-export default getAllReviews;
