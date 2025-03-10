8046e2d1e41b7a9a63cb9caae6b7fd4212bfaeb4
naimurk <naimurk781@gmail.com>
2025-03-10 09:59:10 +0600

    services dynamic data showing

--- src/app/(withMainLayout)/service/page.tsx
+++ src/app/(withMainLayout)/service/page.tsx
@@ -1,19 +1,41 @@
 import assets from "@/assets";
 import { ParallaxBanner, SectionTitle } from "@/components/common";
-import { services } from "./constants/services";
 import { ServiceCard, WhyChooseUs } from "./_components";
+import { TService } from "@/types";
 
-const Service = () => {
+const getServicesData = async () => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/service/get-all-services`,
+      {
+        next: { revalidate: 3000 },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch services: ${res.statusText}`);
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching services:", error);
+    return null;
+  }
+};
+
+const Service = async () => {
+  const services = await getServicesData();
+  const servicesData: TService[] = services?.data || [];
   return (
-    <div className="  ">
+    <div>
       <ParallaxBanner
         img={assets.banner.serviceBanner.src}
         title="Our Services"
         pageTitle="services"
       />
 
-      <div className="container mx-auto  ">
-        {/* section title */}
+      <div className="container mx-auto">
+        {/* Section title */}
         <div className="my-[50px]">
           <SectionTitle
             title="Innovative digital solution services"
@@ -21,17 +43,29 @@ const Service = () => {
           />
         </div>
 
-        {/* services card */}
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 ">
-          {services.map((item, idx) => (
-            <div key={idx} className={`${idx === 2 ? "lg:col-span-2" : ""}`}>
-              <ServiceCard item={item} />
+        {/* Error message if services fail to load */}
+        {!services ? (
+          <div className="text-center text-red-500">
+            Failed to load services. Please try again later.
+          </div>
+        ) : (
+          <>
+            {/* Services card */}
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+              {servicesData?.map((item: TService, idx: number) => (
+                <div
+                  key={idx}
+                  className={`${idx === 2 ? "lg:col-span-2" : ""}`}
+                >
+                  <ServiceCard item={item} />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
 
-        {/*  why choose us */}
-        <WhyChooseUs />
+            {/* Why choose us */}
+            <WhyChooseUs />
+          </>
+        )}
       </div>
     </div>
   );
